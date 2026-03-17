f3fd4d0534485cc70d5e67da287e772f91f573da
Ítalo Oliveira <253392154+Italoliver@users.noreply.github.com>
2026-03-17 18:55:17 -0300

    Add string multiplication functionality

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,11 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+# 1. Recebendo as entradas
+string = input("Digite uma string: ")
+number = int(input("Digite o segundo número: "))
+
+# 2. Realizando a multiplicação do texto
+resultado = string * number
+
+# 3. Exibindo o resultado
+print((string + ' ') * number)
