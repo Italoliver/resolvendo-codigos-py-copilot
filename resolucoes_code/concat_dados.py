048c94ea5192635d4761dc0f792a83bbb81591d2
Ítalo Oliveira <253392154+Italoliver@users.noreply.github.com>
2026-03-17 18:35:28 -0300

    Add user input and string concatenation example

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,12 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+# Recebendo os dados do usuário
+dado1 = input("Digite a primeira parte: ")
+dado2 = input("Digite a segunda parte: ")
+
+# Concatenando as strings
+# Usamos o f"" para inserir as variáveis diretamente no texto
+resultado = dado1 + " " + dado2
+
+# Exibindo o resultado final
+print("Aqui está sua string concatenada: ", resultado)
